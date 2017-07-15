2ae8a6844df7597a5f05db4016169194e1c50230
tanyax <tanyav2@illinois.edu>
2017-07-15 00:53:20 -0500

    heroku-demo

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
 
 app.get('/', function(req, res){
   res.render('index');
 });
 
-app.listen(3000);
-console.log("Listening to port 3000...");
+app.get('/success', function(req, res){
+  res.send('sorry no one found');
+});
+
+app.listen(process.env.PORT || 3000);
+console.log("Listening to port 5000...");
